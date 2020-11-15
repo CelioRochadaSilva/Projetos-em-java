c82a8feb9bb760ad0e8a055d334287e612ff9519
CelioRochadaSilva <celioelea@hotmail.com>
2020-11-14 22:49:17 -0300

    App
    mais exemplos

---
+++ java/Teste_paginajava/tela_java.java
@@ -0,0 +1,10 @@
+public class Pesso{
+    pubçic String nome = 'Celio';
+    
+    pubçic String getNome String {return , nome};
+    
+    pubçic String falarMeuProprioNome(){
+        returm('Meu nome é'+getNome);
+    }
+
+} 
